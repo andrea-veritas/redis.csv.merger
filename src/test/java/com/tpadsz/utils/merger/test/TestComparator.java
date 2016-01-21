609c20285d678c67a3fa92c41b5ceb085f1cdb84
王鑫康 <roger.wang@tpadsz.com>
2016-01-21 22:52:53 +0800

    Tested StringArrayComparator

--- src/test/java/com/tpadsz/utils/merger/test/TestComparator.java
+++ src/test/java/com/tpadsz/utils/merger/test/TestComparator.java
@@ -1,11 +1,20 @@
 package com.tpadsz.utils.merger.test;
 
 
+import com.tpadsz.utils.merger.StringArrayComparator;
+import org.junit.Test;
 
 /**
  * Created by Roger on 2016/1/21.
  */
 
 public class TestComparator {
+    @Test
+    public void testFindMax(){
+        assert (new StringArrayComparator(new String[]{"1","2","3"}).max()==2);
+    }
 
+    public void testFindMin(){
+        assert (new StringArrayComparator(new String[]{"e","b","a","f"}).min()==2);
+    }
 }
